745911ca0559b7e35e9b654d9be9616ae98606d3
Dean Williams <dean.williams1980@gmail.com>
2015-09-13 13:16:52 +0100

    Initial Commit

---
+++ Noughts and Crosses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Noughts and Crosses
+//
+//  Created by Dean Williams on 13/09/2015.
+//  Copyright © 2015 Codemonkeyism. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
